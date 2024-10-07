9cfc9eb149192a4fe72be37578a5d71225998bcf
Nupur S <87416019+nupur710@users.noreply.github.com>
2024-10-07 09:48:04 +0530

    final encoded message

--- errorcorrection.py
+++ errorcorrection.py
@@ -1,24 +1,24 @@
 from encoder import Encoder
 from versionselector import VersionSelector
+from bitarray import bitarray
 
 class ErrorCorrection:
     def __init__(self):
         self.encoder= Encoder()
         self.version= VersionSelector()
-        #error correcting codewords per block
-        self.m_ec_codewords= {
-    1: 10, 2: 16, 3: 26, 4: 18, 5: 24,
-    6: 16, 7: 18, 8: 22, 9: 22, 10: 26,
-    11: 30, 12: 22, 13: 22, 14: 24, 15: 24,
-    16: 28, 17: 28, 18: 26, 19: 26, 20: 26,
-    21: 26, 22: 28, 23: 28, 24: 28, 25: 28,
-    26: 28, 27: 28, 28: 28, 29: 28, 30: 28,
-    31: 28, 32: 28, 33: 28, 34: 28, 35: 28,
-    36: 28, 37: 28, 38: 28, 39: 28, 40: 28
-    }
         self.gf_exp= [0] * 512 #exponent table
         self.gf_log= [0] * 256 #log table
         self.init_galois_field()
+        self.requred_remainder_bits= {
+        1: 0, 2: 7, 3: 7, 4: 7, 5: 7,
+        6: 7, 7: 0, 8: 0, 9: 0, 10: 0,
+        11: 0, 12: 0, 13: 0, 14: 3, 15: 3,
+        16: 3, 17: 3, 18: 3, 19: 3, 20: 3,
+        21: 4, 22: 4, 23: 4, 24: 4, 25: 4,
+        26: 4, 27: 4, 28: 3, 29: 3, 30: 3,
+        31: 3, 32: 3, 33: 3, 34: 3, 35: 0,
+        36: 0, 37: 0, 38: 0, 39: 0, 40: 0
+        }
 
     #initialize log and exponent table
     def init_galois_field(self):
@@ -38,7 +38,7 @@ class ErrorCorrection:
         return self.gf_exp[self.gf_log[x] + self.gf_log[y] % 255]
     
     def construct_generator_polynomial(self, version):
-        num_condewords= self.m_ec_codewords[version]
+        num_condewords= self.encoder.block_info[version][1]
         g= [1]
         for i in range(num_condewords):
             g= self.multiply_polynomials(g, [1, self.gf_exp[i]])
@@ -73,14 +73,63 @@ class ErrorCorrection:
                         remainder[i + j] ^= self.gf_exp[(factor + self.gf_log[generator_poly[j]]) % 255]
         return remainder[-(len(generator_poly) - 1):]
     
+    def split_into_blocks(self, message_poly, version):
+        block_info= self.encoder.block_info[version]
+        num_blocks1= block_info[2]
+        num_blocks2= block_info[3]
+        data_codewords1= block_info[4]
+        data_codewords2= block_info[5]
+        blocks = []
+        start = 0
+        for i in range(num_blocks1):
+            end = start + data_codewords1
+            blocks.append(message_poly[start:end])
+            start = end
+        for i in range(num_blocks2):
+            end = start + data_codewords2
+            blocks.append(message_poly[start:end])
+            start = end
+        return blocks
+    
+    def interleave_blocks(self, data_blocks, ec_blocks):
+        interleaved = []
+        max_length = max(len(block) for block in data_blocks)
+        for i in range(max_length):
+            for block in data_blocks:
+                if i < len(block):
+                    interleaved.append(block[i])
+        max_ec_length = len(ec_blocks[0])
+        for i in range(max_ec_length):
+            for block in ec_blocks:
+                interleaved.append(block[i])    
+        return interleaved
+    
+    def get_final_message(self, message, version):
+        str= ""
+        for num in message:
+            str += format(num, '08b')
+        remainder_bits_to_add= self.requred_remainder_bits[version]
+        for i in range(0, remainder_bits_to_add):
+            str += '0'
+        return bitarray(str)
+        
+    
     def generate_error_correction_codewords(self, text):
         encoding_mode= self.encoder.determine_encoding(text)
         version= self.version.smallest_version(text, encoding_mode)
         generator_poly = self.construct_generator_polynomial(version)
         message_poly = self.message_polynomial(text)
-        error_correction_codewords = self.div(message_poly, generator_poly)
-        
-        return error_correction_codewords
+        #Split message polynomial into blocks
+        data_blocks = self.split_into_blocks(message_poly, version)
+        #error correction codewords for each block
+        ec_blocks = []
+        for block in data_blocks:
+            ec_block = self.div(block, generator_poly)
+            ec_blocks.append(ec_block)
+        #Interleave the data and error correction codewords
+        message = self.interleave_blocks(data_blocks, ec_blocks)
+        final_message= self.get_final_message(message, version)
+        return final_message
 
     
 e= ErrorCorrection()
